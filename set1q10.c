3bc30ac1977efe8efe5ef11eac908c0076a506f7
rasakailasam <35728308+rasakailasam@users.noreply.github.com>
2018-01-30 16:14:04 +0530

    Create set1q10.c

---
+++ set1q10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+long long n;
+int count=0;
+printf("Enter an integer");
+scanf("%lld",&n);
+while(n!=0)
+{
+n/=0;
+++count;
+}
+printf("Number of digits:%d",count);
+}
