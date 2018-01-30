d5c81fbbac95922c152a138fcaa56282442e4401
rasakailasam <35728308+rasakailasam@users.noreply.github.com>
2018-01-30 16:26:23 +0530

    Create set1q3.c

---
+++ set1q3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char word;
+scanf("%c",&word);
+if(word=='a'||word=='e'||word=='i'||word=='o'||word=='u')
+{
+printf("vowel");
+}
+else
+{
+printf("not");
+}
+}
