6f5198f8645b16c9616372920ba0a027f9774f49
rasakailasam <35728308+rasakailasam@users.noreply.github.com>
2018-02-08 16:56:39 +0530

    Create set3q22.c

---
+++ set3q22.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int b,a[10],i,d;
+printf("Enter the size of array:\n");
+scanf("%d",&b);
+printf("Enter the elements of array:\n");
+for(i=0;i<b;i++)
+{
+scanf("%d",&a[i]);
+}
+d=a[0];
+for(i=0;i<b;i++)
+{
+if(a[i]>d)
+{
+d=a[i];
+}}
+printf("Maximum Value is %d",d);
+getch();
+}
