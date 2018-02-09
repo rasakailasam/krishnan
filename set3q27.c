2a86ca9e3a9e5818090274d2231f8ddf98e44431
rasakailasam <35728308+rasakailasam@users.noreply.github.com>
2018-02-09 16:50:26 +0530

    Create set3q27.c

---
+++ set3q27.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int n,a;
+ printf("\n the value of:");
+ scanf("%d",&n);
+ if(n==2)
+ {
+     printf("the value is : Yes");
+ }
+ else(n==a)
+ {
+     printf(" the value is :alphabet");
+ }
+}
