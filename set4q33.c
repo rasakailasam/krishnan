958a5b3b061b0d1c0ae6aa2cf322bc9d6be332da
rasakailasam <35728308+rasakailasam@users.noreply.github.com>
2018-02-22 16:07:07 +0530

    Create set4q33.c

---
+++ set4q33.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	char ch[100];
+	int i,k,count=0;
+	gets(ch);
+	for(k=0;ch[k]!='\0';++k)
+	{
+		
+	}
+	for(i=0;i<k;i++)
+	{
+	
+	if(ch[i]==' ')
+	{
+		count++;
+	}
+		
+	}
+	printf("%d",count);
+	return 0;
+}
