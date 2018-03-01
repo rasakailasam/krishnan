4f48febbef615501d034b3bd36696d651609878a
rasakailasam <35728308+rasakailasam@users.noreply.github.com>
2018-03-01 16:25:03 +0530

    Create set5q42.c

---
+++ set5q42.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char c[100],d[100];
+	int i,n,n1;
+	scanf("%s",c);
+	scanf("%s",d);
+	n=strlen(c);
+	n1=strlen(d);
+    if(n==n1)
+    {
+        printf("%s",d);
+    }
+    else if(n>n1)
+    {
+        printf("%s",d);
+    }
+    else
+    {
+        printf("%s",d);
+    }
+	 return 0;
+}
