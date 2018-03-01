8cb4f3d392a33b27a926aaada3889dafb20eac0c
rasakailasam <35728308+rasakailasam@users.noreply.github.com>
2018-03-01 16:01:33 +0530

    Create set4q38r.c

---
+++ set4q38r.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  printf("Enter the two elements:");
+  scanf("%d\t%d",&a,&b);
+  a^=b;
+  b^=a;
+  a^=b;
+  printf("After swapping %d %d",a,b);
+  return 0;
+}
