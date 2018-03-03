c81752985d16e5ae4b29d5962c3681f66d401051
rasakailasam <35728308+rasakailasam@users.noreply.github.com>
2018-03-03 15:58:31 +0530

    Create set5q48.c

---
+++ set5q48.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int  main()
+{
+int n,a[100],avg,sum=0,i;
+printf("Enter the number:");
+scanf("%d",&n);
+scanf("%d",&a[i]);
+for(i=0;i<=n;i++)
+{
+sum=sum+a[i];
+}
+printf("The sum value is");
+
+return 0;
+}
