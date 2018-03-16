6ef059e4127c37541da8c3fdee45f4f2603b31f1
rasakailasam <35728308+rasakailasam@users.noreply.github.com>
2018-03-16 15:00:13 +0530

    Create set7q68.c

---
+++ set7q68.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("\nEnter the number :");
+    scanf("%d",&n);
+    if(n%7==0)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    return 0;
+}
